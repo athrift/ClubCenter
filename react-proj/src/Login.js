2b9430516942b1ad940ad6473a2f66a80659503a
Alex Thrift <athrift@purdue.edu>
2020-10-26 21:53:35 -0400

    Adds Nav bar functionality to App.js. Old App.js refactored in Login.js

---
+++ react-proj/src/Login.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import axios from 'axios';
+import logo, { ReactComponent } from './logo.svg';
+import './Login.css';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Login extends React.Component {
+
+  state = {
+    username: '',
+    password: '',
+    // posts: []
+  };
+
+  // Function which handles the user input 
+  // i.e when user inputs the username and password
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+  // handleChange = (event) => {
+  //   const target = event.target;
+  //   const name = target.name;
+  //   const value = target.value;
+
+  //   this.setState({ [name]: value });
+  // };
+
+  // Function to reset user input
+  resetUserInputs = () => {
+    this.setState({
+      username: '',
+      password: ''
+    });
+  };
+
+  // Function which handles the chnages when the Submit Button is clicked
+  submit = (event) => {
+    event.preventDefault();
+
+    const payload = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    // Sending the data from the html form to the server
+    axios({
+      url: '/api/save',
+      method: 'POST',
+      data: payload
+    })
+      .then(() => {
+        console.log('Data has been sent to the server');
+        this.resetUserInputs();
+        // this.getBlogPost();
+      })
+      .catch(() => {
+        console.log('Internal server error');
+      });;
+  };
+
+  render() {
+
+    console.log('State: ', this.state);
+
+    return (
+      <div className="Login">
+        <header className="Login-header">
+	  {/* <Container> */}
+          <Form onSubmit={this.submit}>
+            <Form.Group controlId="FormEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="text" name="username" placeholder="example@email.com" value={this.state.username} onChange={this.handleChange} />
+              {/* <Form.Text className="text-muted"></Form.Text> */}
+            </Form.Group>
+            <Form.Group controlId="FormPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="text" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+            </Form.Group>
+            <Button variant="secondary" type="submit">Sign Up</Button>
+          </Form>
+          {/* </Container> */}
+        </header>
+      </div >
+    );
+  }
+
+}
+
+export default Login;
