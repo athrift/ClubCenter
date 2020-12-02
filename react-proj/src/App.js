6e2d9cbf0875e31f1c4891e082eb6a85f7665af1
Isha Mahadalkar <ishamahadalkar03@gmail.com>
2020-12-02 15:47:26 -0500

    Updating the App.js file to link to the different pages

--- react-proj/src/App.js
+++ react-proj/src/App.js
@@ -8,16 +8,42 @@ import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import image from "./Images/clubcenter.png";
 
+// Imports to keep the User Logged In
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
 //Add external pages
 
 import Create from "./components/Create";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
 
 // Redux Management
 import { Provider } from "react-redux";
 import store from "./store";
 
 
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 //Add internal pages
 
 const Home = () => (
@@ -47,8 +73,11 @@ class App extends React.Component {
               </li>
             </ul>
             <Route exact={true} path="/" component={Home} className="Home" />
-            <Route path="/Create" component={Create} className="Create" />
-            <Route path="/Login" component={Login} className="Login" />
+            <Route exact={true} path="/Create" component={Create} className="Create" />
+            <Route exact={true} path="/Login" component={Login} className="Login" />
+            <Switch>
+              <PrivateRoute exact path="/Dashboard" component={Dashboard} className="Dashboard" />
+            </Switch>
           </header>
         </div>
       </Provider>
